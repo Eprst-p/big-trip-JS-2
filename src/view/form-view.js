cbe53848aa7c9a727e90b5a57978664eaf7bf4b6
Eprst <easter_p@mail.ru>
2021-11-28 13:40:52 +0300

    Уменьшил простыню типов

--- src/view/form-view.js
+++ src/view/form-view.js
@@ -1,52 +1,25 @@
-import {OFFER_NAMES, PRICES} from '../mock/data-sources.js';
+import {OFFER_NAMES, PRICES, POINT_TYPES} from '../mock/data-sources.js';
 
 
-const createTypeListTemplate = () => (
-  `<div class="event__type-item">
-    <input id="event-type-taxi-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="taxi">
-    <label class="event__type-label  event__type-label--taxi" for="event-type-taxi-1">Taxi</label>
-  </div>
+const createTypeListTemplate = () => {
 
-  <div class="event__type-item">
-    <input id="event-type-bus-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="bus">
-    <label class="event__type-label  event__type-label--bus" for="event-type-bus-1">Bus</label>
-  </div>
+  let readyTypeList = '';
+  const renderType = (type) => {
+    const lowerType = type.toLowerCase();
+    return (
+      `<div class="event__type-item">
+        <input id="event-type-${lowerType}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${lowerType}">
+        <label class="event__type-label  event__type-label--${lowerType}" for="event-type-${lowerType}-1">${type}</label>
+      </div>`
+    );
+  };
 
-  <div class="event__type-item">
-    <input id="event-type-train-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="train">
-    <label class="event__type-label  event__type-label--train" for="event-type-train-1">Train</label>
-  </div>
+  POINT_TYPES.forEach((currentType) => {
+    readyTypeList += renderType(currentType);
+  });
 
-  <div class="event__type-item">
-    <input id="event-type-ship-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="ship">
-    <label class="event__type-label  event__type-label--ship" for="event-type-ship-1">Ship</label>
-  </div>
-
-  <div class="event__type-item">
-    <input id="event-type-drive-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="drive">
-    <label class="event__type-label  event__type-label--drive" for="event-type-drive-1">Drive</label>
-  </div>
-
-  <div class="event__type-item">
-    <input id="event-type-flight-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="flight" checked>
-    <label class="event__type-label  event__type-label--flight" for="event-type-flight-1">Flight</label>
-  </div>
-
-  <div class="event__type-item">
-    <input id="event-type-check-in-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="check-in">
-    <label class="event__type-label  event__type-label--check-in" for="event-type-check-in-1">Check-in</label>
-  </div>
-
-  <div class="event__type-item">
-    <input id="event-type-sightseeing-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="sightseeing">
-    <label class="event__type-label  event__type-label--sightseeing" for="event-type-sightseeing-1">Sightseeing</label>
-  </div>
-
-  <div class="event__type-item">
-    <input id="event-type-restaurant-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="restaurant">
-    <label class="event__type-label  event__type-label--restaurant" for="event-type-restaurant-1">Restaurant</label>
-  </div>`
-);
+  return readyTypeList;
+};
 
 const createTypeAndCityTextTemplate = (_type, city) => (
   `<div class="event__field-group  event__field-group--destination">
