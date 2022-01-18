d212aa8a7c17e3c286c5a06cbc3133876b8aceea
Eprst <easter_p@mail.ru>
2022-01-18 18:54:21 +0300

    коррекция фильтров

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,10 +1,12 @@
 import {FilterType}  from './constants.js';
 import {getDateInDayjs} from './time-and-date.js';
 
+const getUnidentifiedByTimePoint = (point) => getDateInDayjs(point.dateFrom) < getDateInDayjs() && getDateInDayjs(point.dateTo) > getDateInDayjs();
+
 const filterFunctional = {
   [FilterType.EVERYTHING]: (points) => points,
-  [FilterType.FUTURE]: (points) => points.filter((point) => getDateInDayjs(point.dateFrom) > getDateInDayjs()),
-  [FilterType.PAST]: (points) => points.filter((point) => getDateInDayjs(point.dateFrom) < getDateInDayjs()),
+  [FilterType.FUTURE]: (points) => points.filter((point) => getDateInDayjs(point.dateFrom) >= getDateInDayjs() || getUnidentifiedByTimePoint(point)),
+  [FilterType.PAST]: (points) => points.filter((point) => getDateInDayjs(point.dateTo) < getDateInDayjs() || getUnidentifiedByTimePoint(point)),
 };
 
 export {filterFunctional};
